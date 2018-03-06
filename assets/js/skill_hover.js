2f0cb9e9fcf7d5f567343c62594a12474a2269b3
locphan2207 <locphan2207@gmail.com>
2018-03-05 20:19:36 -0800

    add projects

--- assets/js/skill_hover.js
+++ assets/js/skill_hover.js
@@ -1,12 +1,21 @@
 let skill = document.getElementsByClassName('skill');
-console.log(skill);
 for (let i = 0; i < skill.length; i++) {
   skill[i].addEventListener('mouseenter', (e) => {
-    console.log(e);
     e.target.getElementsByTagName('span')[0].style += ";opacity: 1;";
   });
   skill[i].addEventListener('mouseleave', (e) => {
-    console.log(e);
     e.target.getElementsByTagName('span')[0].style -= ";opacity: 1;";
+  });
+}
+
+let projects = document.getElementsByClassName('project-screenshot-container');
+for (let i = 0; i < skill.length; i++) {
+  projects[i].addEventListener('mouseenter', (e) => {
+    e.target.getElementsByClassName('screenshot-view')[0].style += ";display:inline";
+    e.target.getElementsByClassName('screenshot-gradient')[0].style += ";background: rgba(0,0,0,0.8)";
+  });
+  projects[i].addEventListener('mouseleave', (e) => {
+    e.target.getElementsByClassName('screenshot-view')[0].style -= ";display:inline";
+    e.target.getElementsByClassName('screenshot-gradient')[0].style -= ";background: rgba(0,0,0,0.8)";
   });
 }
